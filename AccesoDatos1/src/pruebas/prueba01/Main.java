af5d6a5ef195a0b3276f2a835f48065fb18656d7
alumno <alumno@DAM220>
2025-09-18 10:00:44 +0200

    www

---
+++ AccesoDatos1/src/pruebas/prueba01/Main.java
@@ -0,0 +1,41 @@
+package pruebas.prueba01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		List<Alumno> alumnos = new ArrayList<>(
+				Arrays.asList(new Alumno(5555555, "Candela", "Ramirez", "DAW", "Primero", "D2", 'f', "21/09/2004"),
+						new Alumno(2222222, "Fernanda", "Martinez", "DAW", "Segundo", "A2", 'f', "15/06/2003"),
+						new Alumno(1111111, "Javier", "Gonzalez", "DAM", "Primero", "C1", 'm', "03/04/2004"),
+						new Alumno(4444444, "Fran", "Fernandez", "DAM", "Segundo", "C3", 'm', "09/01/2002"),
+						new Alumno(3333333, "Carla", "Lopez", "ASIR", "Primero", "B1", 'f', "28/11/2005")));
+
+		for (Alumno a : alumnos) {
+			System.out.println(a + "\n");
+		}
+
+		Comparator<Alumno> compararNia = new Comparator<Alumno>() {
+			@Override
+			public int compare(Alumno a1, Alumno a2) {
+				return a1.getNia() - a2.getNia();
+			}
+		};
+
+		Collections.sort(alumnos, compararNia);
+
+		System.out.println("Alumnos ordenados por NIA: \n");
+		for (Alumno a : alumnos) {
+			System.out.println(a + "\n");
+		}
+		
+		
+	}
+
+}
