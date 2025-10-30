f21e7397d861563a466db80988434db4024a39e1
alumno <alumno@DAM220>
2025-10-30 09:46:03 +0100

    fgfdsa

--- AccesoDatos1/src/pruebas/prueba03/Ejercicio02.java
+++ AccesoDatos1/src/pruebas/prueba03/Ejercicio02.java
@@ -12,26 +12,39 @@ public class Ejercicio02 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		String fichero, fichero2, fichero3, linea, linea2;
+		String fichero, fichero2, fichero3, linea = "", linea2 = "";
 
 		System.out.println("Que fichero quieres leer");
-		fichero = sc.nextLine();
+		fichero = "ejercicio2.1.txt";
 
 		System.out.println("Que otro fichero quieres leer");
-		fichero2 = sc.nextLine();
+		fichero2 = "ejercicio2.txt";
 
 		System.out.println("En que fichero quieres meter las lineas mezcladas");
-		fichero3 = sc.nextLine();
+		fichero3 = "lineasMezcladas.txt";
 
 		try (BufferedReader br = new BufferedReader(new FileReader(fichero));
 				BufferedReader br2 = new BufferedReader(new FileReader(fichero2));
 				PrintWriter pw = new PrintWriter(new FileWriter(fichero3))) {
 
 			while ((linea = br.readLine()) != null && (linea2 = br2.readLine()) != null) {
-
 				pw.println(linea);
 				pw.println(linea2);
+
 			}
+
+			//if (linea == null) {
+				while ((linea2 = br2.readLine()) != null) {
+					pw.println(linea2);
+				}
+			//}
+
+			//if (linea2 == null) {
+				pw.println(linea);
+				while ((linea = br.readLine()) != null) {
+					pw.println(linea);
+				}
+			//}
 
 		} catch (IOException e) {
 			System.out.println("Error al leer el archivo: " + e.getMessage());
